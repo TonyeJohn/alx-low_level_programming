6de8130ee1e023140fcb895044ba995007d1fe23
TonyeJohn <john.tonye28@yahoo.com>
2022-09-09 12:48:33 -0700

    task 102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+/**
+ * main - print possible combination of two numbers
+ *
+ * Return: returns zero at the end
+ */
+int main(void)
+{
+int a,b;
+for (a = 0; a < 100; a++)
+{
+for (b = 0; b < 100; j++)
+{
+if (b > a)
+{
+putchar((a / 10) + '0');
+putchar((a % 10) + '0');
+putchar(' ');
+putchar((b / 10) + '0');
+putchar((b % 10) + '0');
+if (a != 98)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
