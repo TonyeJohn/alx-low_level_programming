f9504d2224bbe37b72fe3cd2138e6699f481a760
TonyeJohn <john.tonye28@yahoo.com>
2022-09-09 11:23:05 -0700

    task 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,23 +1,56 @@
 #include <stdio.h>
-/**
- * main - printing numbers from 0-9 with commas and space between them
- * Description: using the main function
- * this program prints "0, 1, 2, 3, 4, 5, 6, 7, 8, 9"
- * Return: 0
- */
-int main(void)
-{
-	int c;
 
-	for (c = 48; c <= 57; c++)
-	{
-		putchar(c);
-		if (c != 57)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-	}
-	putchar('\n');
-	return (0);
+/**
+
+ * main - print single digit numbers starting from 0
+
+ *
+
+ * Return: returns zero at the end
+
+ */
+
+int main(void)
+
+{
+
+        int i, j;
+
+
+        for (i = 0; i <= 9; i++)
+
+        {
+
+                for (j = 1; j <= 9; j++)
+
+                {
+
+                        if (j > i)
+
+                        {
+
+                                putchar(i + '0');
+
+                                putchar(j + '0');
+
+                                if (i != 8)
+
+                                {
+
+                                        putchar(',');
+
+                                        putchar(' ');
+
+                                }
+
+                        }
+
+                }
+
+        }
+
+        putchar('\n');
+
+        return (0);
+
 }
