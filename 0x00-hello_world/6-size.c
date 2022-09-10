4f184b22c7889a76c6c69f6abbd1615ed0835451
TonyeJohn <john.tonye28@yahoo.com>
2022-09-10 01:26:15 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,12 +12,10 @@ int i;
 long lg;
 long llg;
 float f;
-double db;
 printf("Size of a char: %d byte(s)\n", sizeof(c));
 printf("Size of an int: %d byte(s)\n", sizeof(i));
 printf("Size of a long int: %d byte(s)\n", sizeof(lg));
-printf("Size of a long long int: %d byte(s)\n", sizeof(llg));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(llg));
 printf("Size of a float: %d byte(s)\n", sizeof(f));
-printf("Size of a double: %lu byte(s)\n", sizeof(db));
 return (0);
 }
