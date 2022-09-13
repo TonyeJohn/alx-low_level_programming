de12704869aa6bd98b672ce7ac8c866475b83444
TonyeJohn <john.tonye28@yahoo.com>
2022-09-13 11:54:39 -0700

    task 101

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,11 +8,11 @@ int main(void)
 {
 int sum = 0;
 int i;
-for (i=3; i<1024; i++)
+for(i=3; i<1024; i++)
 {
-	if ((i%3==0) || (i%5==0))
-	sum += i;
+if ((i%3==0) || (i%5==0))
+sum += i;
 }
 printf("%d\n", sum);
-return 0;
+return (0);
 }
