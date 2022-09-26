5941d19c4f0ddeb2b624696a9b083a231b347d01
TonyeJohn <john.tonye28@yahoo.com>
2022-09-26 14:24:23 -0700

    task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,19 +10,16 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-	unsigned int i, j;
+int index;
 
-	for (i = 0; s[i]; i++)
-	{
-		for (j = 0; accept[j]; j++)
-		{
-			if (s[i] == accept[j])
-			{
-				break;
-			}
-			if (accept[j])
-				return (s + i);
-		}
-		return (0);
-	}
+while (*s)
+}
+for (index = 0; accept[index]; index++)
+}
+if (*s == accept[index])
+return (s);
+}
+s++;
+}
+return (0);
 }
